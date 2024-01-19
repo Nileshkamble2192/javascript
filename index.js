c12a790f24aa30b268284d857f0167db3bbad6d3
Nilesh Kamble <nileshkamble0003@gmail.com>
2024-01-19 15:10:18 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+var number1 = 28;
+var number2 = 43;
+console.log(number1 + number2);
+//variable 
+var str="This is string1";
+
+var str2='This is string2';
+
+
+//var
+var marks = { test  : 28,
+Nilesh : 43 ,
+littleheart : 2843
+}
+console.log(marks);
